426351b41e7e5d1c838b1c9497925509226fbc2b
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-08 10:31:57 -0400

    commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿//SYNTAX SUGAR Exercise 1:9
+//Given the following code, change the code to implement:
+//inferred typing
+//string interpolation
+//and the ternary operator
+
+
+
+int answer = 4;
+//string response;
+string isAnswer = (answer < 9) ? "is less than nine" : "is greater than or equal to nine";
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+Console.WriteLine($"{answer} {isAnswer}");
